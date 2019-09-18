9e3fc0426a69892de506896c75745b721cffa060
Harmen Arts <harmen.arts@ida-mediafoundry.nl>
2019-09-18 17:26:08 +0200

    Fixed imports of GraphClientImpl

--- src/main/java/com/adobe/cq/commerce/graphql/client/impl/GraphqlClientImpl.java
+++ src/main/java/com/adobe/cq/commerce/graphql/client/impl/GraphqlClientImpl.java
@@ -21,7 +21,12 @@ import java.util.List;
 
 import javax.net.ssl.SSLContext;
 
-import org.apache.http.*;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpUriRequest;
